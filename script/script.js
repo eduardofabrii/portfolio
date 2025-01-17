633604972a62107219112caacec503df9c63e604
Eduardo Fabri <eduardohfabri@gmail.com>
2025-01-17 09:05:17 -0300

    Implementar Scroll Reveal

---
+++ script/script.js
@@ -0,0 +1,24 @@
+var nodeArray = [
+    document.querySelector('#header'),
+    document.querySelector('#hello-text'),
+    document.querySelector('#im-text'),
+    document.querySelector('#name-text'),
+    document.querySelector('#job-text'),
+    document.querySelector('#button'),
+    document.querySelector('#about'),
+    document.querySelector('#tech'),
+    document.querySelector('#projects'),
+    document.querySelector('#projects-1'),
+    document.querySelector('#projects-2'),
+    document.querySelector('#contact-form'),
+    document.querySelector('#navigation'),
+    document.querySelector('#social-icons'),
+];
+
+ScrollReveal().reveal(nodeArray, { 
+    duration: 1000,
+    origin: 'bottom',
+    distance: '10px', // distância do movimento
+    easing: 'ease-in-out',
+    reset: true // permite que a animação aconteça novamente ao rolar para cima
+});
