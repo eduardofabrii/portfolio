850146fe32074efc11b791de6e7255f66f7d159e
Eduardo Fabri <eduardohfabri@gmail.com>
2025-01-18 14:45:46 -0300

    Ajusta Scroll Reveal

--- script/script.js
+++ script/script.js
@@ -1,27 +1,51 @@
 var nodeArray = [
     document.querySelector('#hello-text'),
+    document.querySelector('#greeting-text'),
     document.querySelector('#im-text'),
     document.querySelector('#name-text'),
     document.querySelector('#job-text'),
-    document.querySelector('#button'),
-    document.querySelector('#about'),
+    document.querySelector('#btnn'),
     document.querySelector('#photo'),
+    document.querySelector('#text-center'),
     document.querySelector('#about-me-sec'),
     document.querySelector('#text-aboutme'),
     document.querySelector('#cv'),
     document.querySelector('#tech'),
-    document.querySelector('#projects'),
-    document.querySelector('#projects-1'),
-    document.querySelector('#projects-2'),
     document.querySelector('#contact-form'),
     document.querySelector('#navigation'),
     document.querySelector('#social-icons'),
 ];
 
+var leftToCenter = [
+    document.querySelector('#projects-img-1'),
+    document.querySelector('#projects-desc-2'),
+];
+
+var rightToCenter = [
+    document.querySelector('#projects-desc-1'),
+    document.querySelector('#projects-img-2'),
+];
+
 ScrollReveal().reveal(nodeArray, { 
-    duration: 1000,
+    duration: 1500,
     origin: 'bottom',
-    distance: '10px', // distância do movimento
+    scale: 0.9, // escala inicial menor para dar efeito de zoom
     easing: 'ease-in-out',
     reset: true // permite que a animação aconteça novamente ao rolar para cima
+});
+
+ScrollReveal().reveal(leftToCenter, { 
+    duration: 2000, 
+    origin: 'left',
+    distance: '100%', 
+    easing: 'ease-in-out',
+    reset: true 
+});
+
+ScrollReveal().reveal(rightToCenter, { 
+    duration: 2000, 
+    origin: 'right',
+    distance: '100%', 
+    easing: 'ease-in-out',
+    reset: true 
 });
